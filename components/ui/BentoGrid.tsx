c606a700c1ef0528f2f91d5cbaeb0ba45968d075
Soranosuke2001 <sorazora1.4@gmail.com>
2024-07-08 15:09:04 +0900

    changed styling

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -20,12 +20,14 @@ export const BentoGrid = ({
 };
 
 export const BentoGridItem = ({
+  id,
   className,
   title,
   description,
   header,
   icon,
 }: {
+  id: number;
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
@@ -35,7 +37,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 border-red-500",
         className
       )}
     >
